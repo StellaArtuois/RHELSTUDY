c9bba0c4c4a422844d39913b6158a3fb189937e4
StellaArtuois <guillermozaandam@hotmail.com>
2019-06-30 21:49:32 +0200

    added --network=bridge:virbr0

--- Configuringvirtualmachine.sh
+++ Configuringvirtualmachine.sh
@@ -3,4 +3,4 @@ virt-install --network=bridge:virbr0 --name=test1.example.com --ram=2048 --vcpus
 
 
 ## Configuring virtual machine with kickstart and installation files from a mirror.
-virt-install --name=test1.example.com --ram=2048 --vcpus=2 --disk path=/home/guillermo/SAMSUNGEVO/KVMQEMU/tester1.example.com.qcow2,size=16,bus=virtio --graphics=spice -l https://ams.edge.kernel.org/centos/7.6.1810/os/x86_64/ --os-type=linux --os-variant=centos7.0 -x "ks=ftp://192.168.122.74/pub/ks.cfg"
+virt-install --network=bridge:virbr0 --name=test1.example.com --ram=2048 --vcpus=2 --disk path=/home/guillermo/SAMSUNGEVO/KVMQEMU/tester1.example.com.qcow2,size=16,bus=virtio --graphics=spice -l https://ams.edge.kernel.org/centos/7.6.1810/os/x86_64/ --os-type=linux --os-variant=centos7.0 -x "ks=ftp://192.168.122.74/pub/ks.cfg"
