126cf8f0c463afb0c3f60bdb3963eee9eb0cc431
Guillermo <guillermozaandam@hotmail.com>
2020-04-26 17:03:12 +0200

    deleted the -qq argument in environment.sh

--- Environment.sh
+++ Environment.sh
@@ -14,7 +14,7 @@ sudo systemctl start getty@tty1.service &&
 sudo systemctl enable getty@tty1.service &&
 
 # Apache
-sudo yum install -y -qq -y -qq httpd &&
+sudo yum install -y -y -qq httpd &&
 sudo systemctl start httpd &&
 sudo systemctl enable httpd &&
 sudo mkdir /var/www/html/inst &&
@@ -25,7 +25,7 @@ sudo systemctl restart httpd &&
 sudo systemctl enable httpd &&
 
 # VSFTP
-sudo yum install -y -qq vsftpd &&
+sudo yum install -y vsftpd &&
 sudo systemctl start vsftpd &&
 sudo systemctl enable vsftpd &&
 sudo firewall-cmd --permanent --add-service=ftp &&
@@ -41,7 +41,7 @@ sudo systemctl enable vsftpd &&
 
 # Yum  yum-utils - classic YUM utilities implemented as CLI shims on top of DNF
 
-sudo yum install -y -qq yum-utils &&
+sudo yum install -y yum-utils &&
 
 # Installing Virt-manager and KVM
 #sudo yum update -y &&
@@ -61,37 +61,37 @@ sudo yum install -y -qq yum-utils &&
 #   Remote Management for Linux
 #   Virtualization Platform
 
-sudo yum install -y -qq yum group install "Virtualization Host"
+sudo yum install -y yum group install "Virtualization Host"
 
 # Program to configure kickstart files
-sudo yum install -y -qq system-config-kickstart &&
+sudo yum install -y system-config-kickstart &&
 
 # Terminal Browser
-sudo yum install -y -qq elinks &&
+sudo yum install -y elinks &&
 
 # SeLinux Troubleshoot program
-sudo yum install -y -qq setroubleshoot setools &&
+sudo yum install -y setroubleshoot setools &&
 
 # Cockpit is a web-based server manager https://cockpit-project.org/
 
-sudo yum install -y -qq cockpit &&
+sudo yum install -y cockpit &&
 
-sudo yum install -y -qq rsync &&
+sudo yum install -y rsync &&
 
-sudo yum install -y -qq pykickstart &&
+sudo yum install -y pykickstart &&
 
 # Auditing with Auditd Tool on CentOS/RHEL https://www.tecmint.com/linux-system-auditing-with-auditd-tool-on-centos-rhel/
 
-sudo yum install -y -qq auditd &&
+sudo yum install -y auditd &&
 
 # Autofs for automatically mounting directories at request basis instead of automatically when the server starts.
 # "Autofs uses the automount daemon to manage your mount points by only mounting them dynamically when they are accessed."
 
-sudo yum install -y -qq autofs &&
+sudo yum install -y autofs &&
 
 # Lynis is one of the most trusted automated auditing tool for software patch management, malware scanning and vulnerability detecting in Unix/Linux based systems. (https://cisofy.com/)
 
-sudo yum install -y -qq lynis &&
+sudo yum install -y lynis &&
 
 
 # SELinux Policy Management tool.
